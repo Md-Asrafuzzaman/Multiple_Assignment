d641b76c4d18edafff27e85608f2ba3ae5be9e5e
jaman <jamanswe95@gmail.com>
2019-10-16 01:17:28 +0600

    Assignment11 Commit

---
+++ RefrigeratorAppPractice3/RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        private Refrigetor refrigetor; 
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            refrigetor = new Refrigetor(Convert.ToDouble(maxWeightTakeTextBox.Text));
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            refrigetor.item.Add(Convert.ToInt16(itemTextBox.Text));
+            refrigetor.weight.Add(Convert.ToDouble(weightTextBox.Text));
+            if (refrigetor.Valid())
+            {
+               currentWeightTextBox.Text= refrigetor.CW().ToString();
+               remainingWeightTextBox.Text = refrigetor.RW().ToString();
+            }
+
+        }
+    }
+}
---
+++ RefrigeratorAppPractice3/RefrigeratorAppPractice3/Refrigetor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    public class Refrigetor
+    {
+        public double maxWeight;
+        public List<int> item = new List<int>();
+        public List<double> weight = new List<double>();
+
+
+        public Refrigetor(double maxWeight)
+        {
+            this.maxWeight = maxWeight;
+        }
+
+        public double CW()
+        {
+            double cw = 0;
+            for (int i = 0; i < item.Count; i++)
+            {
+                cw = item[i] * weight[i];
+            }
+
+            return cw;
+        }
+
+        public double RW()
+        {
+            return maxWeight - CW();
+        }
+
+        public bool Valid()
+        {
+            if (RW() > maxWeight)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
